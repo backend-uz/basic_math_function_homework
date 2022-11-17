d5f2fb1168978179675cd5dde909e1519cfb3f50
user1 <backenduz@gmail.com>
2022-11-17 14:59:23 +0500

    done

--- basic_func14.py
+++ basic_func14.py
@@ -1,11 +1,12 @@
 def main(a, b):
+    from math import floor
     '''find the floor division of a and b and return it.
-    
     Args:
         a (int): a number
         b (int): a number
-        
     Returns:
         int: the result.
     '''
-    return 
+    c = floor(a/b)
+    return c
+print(main(11, 2))
